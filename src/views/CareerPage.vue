1bce07f47512a9f7455f7558e56d1e98bb1a4808
midoriq <izuku.midoriq22@gmail.com>
2022-10-25 23:05:55 +0200

    Adding styles to Career Item Page

--- src/views/CareerPage.vue
+++ src/views/CareerPage.vue
@@ -11,6 +11,7 @@ export default {
     data() {
         return {
             career: 0,
+            others: [],
         };
     },
     methods: {
@@ -18,7 +19,12 @@ export default {
             for (var i = 0; i < Kariera.kariera.length; i++) {
                 if (Kariera.kariera[i].id == this.$route.params.id) {
                     console.log(this.career);
-                    return (this.career = Kariera.kariera[i]);
+                    this.career = Kariera.kariera[i];
+                } else {
+                    this.others.push({
+                        name: Kariera.kariera[i].name,
+                        desc: Kariera.kariera[i].description,
+                    });
                 }
             }
         },
@@ -31,18 +37,156 @@ export default {
 
 <template>
     <Navbar class="nav"> </Navbar>
-    <div class="content">
-        <h1>{{ career.name }}</h1>
+    <div class="main">
+        <div class="content">
+            <div class="header">
+                <h1>{{ career.name }}</h1>
+            </div>
+
+            <div class="desc">
+                <p>{{ career.description }}</p>
+                <div class="needs">
+                    <p>
+                        <span class="header2">Technologie: </span>
+                        {{ career.technologies }}
+                    </p>
+                    <p>
+                        <span class="header2">Prowadzenie projektu: </span>
+                        {{ career.projects }}
+                    </p>
+                    <p>
+                        <span class="header2">Mile widziane: </span>
+                        {{ career.milewidziane }}
+                    </p>
+                    <p>
+                        <span class="header2">Umowa: </span>
+                        {{ career.contract }}
+                    </p>
+                    <p>
+                        <span class="header2">Wymiar czasu pracy: </span>
+                        {{ career.times }}
+                    </p>
+                    <p>
+                        <span class="header2">Zadania: </span>
+                        {{ career.tasks }}
+                    </p>
+                </div>
+            </div>
+            <p class="addit">
+                Zainteresowane osoby prosimy o wysłanie CV ze zdjęciem na adres
+                mailowy: rekrutacja at masterdev.pl lub o kontakt
+                telefoniczny.<br />
+                W tytule maila prosimy wpisać JUNIOR .NET DEVELOPER oraz swoje
+                IMIĘ i NAZWISKO np. JUNIOR .NET DEVELOPER JAN KOWALSKI
+            </p>
+            <p class="claus">
+                Prosimy o umieszczenie w CV klauzuli "Wyrażam zgodę na
+                przetwarzanie moich danych osobowych dla potrzeb niezbędnych do
+                rekrutacji (Zgodnie z Ustawą z dnia 29.08.1997r. o ochronie
+                danych osobowych Dz. U. Nr 133, poz 883).")
+            </p>
+        </div>
+        <div class="others" v-if="others.length > 0">
+            <h4>Pozostałe oferty pracy</h4>
+            <div class="otheritem" v-for="other in others">
+                <h5>{{ other.name }}</h5>
+                <p>{{ other.desc }}</p>
+            </div>
+        </div>
     </div>
-    <div class="sidebar">
-        <h2>Nasze inne oferty</h2>
-        <!-- Tutaj dać komponent żeby wyświetlał -->
-    </div>
+
     <Footer class="foot"></Footer>
 </template>
 
 <style scoped>
 .nav {
     position: relative;
+}
+
+h1 {
+    font-family: var(--font-header);
+    font-weight: var(--font-weight-1);
+    font-size: 40px;
+}
+
+h4 {
+    font-family: var(--font-header);
+    font-weight: 600;
+}
+
+h5 {
+    font-family: var(--font-header);
+    font-weight: var(--font-weight-1);
+}
+p {
+    font-family: var(--font-text);
+    text-align: justify;
+}
+
+.main {
+    display: flex;
+    flex-direction: wrap;
+    padding: 70px 0px;
+    justify-content: center;
+    align-items: flex-start;
+}
+
+.content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    width: 65%;
+    gap: 20px;
+}
+
+.desc {
+    line-height: 27px;
+    font-size: 16px;
+}
+
+.content p {
+    width: 81%;
+}
+
+.needs {
+    line-height: 44px;
+}
+.header2 {
+    font-weight: var(--font-weight-1);
+}
+
+.addit {
+    font-size: 14px;
+}
+
+.claus {
+    font-size: 13px;
+}
+
+.others {
+    width: 20%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    color: var(--additional-color);
+}
+.otheritem {
+    width: 100%;
+    background-color: var(--primary-color);
+    color: var(--secondary-color);
+    padding: 30px 40px;
+    box-shadow: 0px 3.01659px 3.01659px rgba(0, 0, 0, 0.25);
+    border-radius: 4px;
+
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+.others p {
+    font-size: 11px;
+}
+.otheritem h5 {
+    font-size: 16px;
 }
 </style>
