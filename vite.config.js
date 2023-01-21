bcd67b4aded3ffa09bdc35b5113cbb896419c300
ola <83216497+midoriq@users.noreply.github.com>
2023-01-21 15:08:17 +0100

    Configuration for hosting

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Praktyki-pazdziernik-2022/'
+  base: '/Praktyki-pazdziernik-2022/',
   plugins: [vue()],
   resolve: {
     alias: {
